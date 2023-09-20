fd6ce64939b2423e2ff4c2eda75de0b64408a743
Junior Plantier <junior.plantier@gmail.com>
2023-09-20 03:10:46 -0300

    feat: endpoint para obter apenas um motorista

--- src/main/kotlin/com/plantier/decarro/interfaces/DriverAPI.kt
+++ src/main/kotlin/com/plantier/decarro/interfaces/DriverAPI.kt
@@ -2,11 +2,14 @@ package com.plantier.decarro.interfaces
 
 import com.plantier.decarro.domain.Driver
 import com.plantier.decarro.domain.DriverRepository
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @Service
 @RestController
@@ -17,4 +20,10 @@ class DriverAPI(
 
     @GetMapping("/drivers")
     fun listDrivers() = driverRepository.findAll()
+
+    @GetMapping("/drivers/{id}")
+    fun findDriver(@PathVariable("id") id: Long) =
+        driverRepository.findById(id)
+            .orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND) }
+
 }
