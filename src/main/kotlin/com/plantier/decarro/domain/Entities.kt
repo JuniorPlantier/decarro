9802c1435ebbe21f421cdf299aa7148e77960fe7
Junior Plantier <junior.plantier@gmail.com>
2023-09-20 03:22:56 -0300

    feat: add POST to add driver

--- src/main/kotlin/com/plantier/decarro/domain/Entities.kt
+++ src/main/kotlin/com/plantier/decarro/domain/Entities.kt
@@ -1,12 +1,14 @@
 package com.plantier.decarro.domain
 
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import java.time.LocalDate
 
 @Entity
 data class Driver(
     @Id
+    @GeneratedValue
     var id: Long? = null,
     val name: String,
     val birthDate: LocalDate
